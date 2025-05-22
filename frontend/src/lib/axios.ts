8b10d70e899fa55babb1193ee43e1a0ba01948c3
piyushkhurana8744 <piyushkhurana8744@gmail.com>
2025-05-22 12:40:23 +0530

    frontend url added

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://piyush.surajahirwar.me/api',
   headers: {
     'Content-Type': 'application/json',
   },
